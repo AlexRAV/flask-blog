f0c226e28744256610ea8bf87b864c9f7b872863
Rebenkov Aleksey <rebenkovaleksey@yandex.ru>
2017-02-03 21:19:45 +0600

    change settings

--- app/settings.py
+++ app/settings.py
@@ -22,7 +22,7 @@ class ProdConfig(Config):
 
     ENV = 'prod'
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:@localhost:5432'  # TODO: Change me
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://postgres:@localhost:5432')
     DEBUG_TB_ENABLED = False  # Disable Debug toolbar
 
 
@@ -34,7 +34,7 @@ class DevConfig(Config):
     DB_NAME = 'dev.db'
     # Put the db file in project root
     DB_PATH = os.path.join(Config.PROJECT_ROOT, DB_NAME)
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_CONNECTION', 'postgresql://postgres:@localhost:5432')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://postgres:@localhost:5432')
     DEBUG_TB_ENABLED = True
     ASSETS_DEBUG = True  # Don't bundle/minify static assets
     CACHE_TYPE = 'simple'  # Can be "memcached", "redis", etc.
